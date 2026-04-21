83529cf6be307a9870c06c8d708083f129bb43c7
Eslam Gamal <eslam.gamal@recapet.com>
2026-04-21 19:38:17 +0200

    edit task

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -30,10 +30,28 @@ const Column = ({ id, title, tasks = [] }) => {
     );
   };
 
+  const onDeleteHandler = () => {
+    if (window.confirm(`Are you sure you want to delete this "${title}"?`)) {
+      setData((prev) =>
+        produce(prev, (draft) => {
+          draft[selectedBoardIndex].columns = draft[
+            selectedBoardIndex
+          ].columns.filter((column) => column.id !== id);
+        }),
+      );
+    }
+  };
+
   return (
     <div className="bg-lines-light flex w-72 shrink-0 flex-col self-start rounded-lg px-2 shadow">
       <h2 className="group/column bg-lines-light text-heading-s relative top-0 rounded px-2 py-4">
         {title} ({tasks?.length})
+        <button
+          className="text-body-m text-red absolute top-0 right-0 bottom-0 p-2 opacity-0 duration-300 group-hover/column:opacity-100 focus:opacity-100"
+          onClick={onDeleteHandler}
+        >
+          Delete
+        </button>
       </h2>
       <div className="mb-5 flex flex-col gap-5">
         {tasks.map((item) => (
